ae20fe70e08813bad9cdda97837a1122f0621b03
Joel Christensen <christensenjo14@gmail.com>
2024-08-04 15:25:03 -0600

    Setup routes

--- main.go
+++ main.go
@@ -94,11 +94,47 @@ func updateReservation(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Reservation not found", http.StatusNotFound)
 }
 
+func deleteReservation(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	for i, reservation := range reservations {
+		if reservation.ID == id {
+			reservations = append(reservations[:i], reservations[i+1:]...)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+
+	http.Error(w, "Reservation not found", http.StatusNotFound)
+}
+
 func main() {
 	// Define routes and handlers here
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Welcome to Dinner Reservations API"))
+	})
+	http.HandleFunc("/reservations", getReservations)
+	http.HandleFunc("/reservation", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			createReservation(w, r)
+		case http.MethodGet:
+			getReservation(w, r)
+		case http.MethodPut:
+			updateReservation(w, r)
+		case http.MethodDelete:
+			deleteReservation(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
 	})
 
 	// Start the HTTP server
